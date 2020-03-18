23125f65010c0e90571dd7f930da9c0514f9f022
Uros Zoretic <uz1734@student.uni-lj.si>
2020-03-18 17:39:52 +0100

    minnor fixes

--- api/src/main/java/si/fri/mag/mappers/NotFoundExceptionMapper.java
+++ api/src/main/java/si/fri/mag/mappers/NotFoundExceptionMapper.java
@@ -14,6 +14,6 @@ public class NotFoundExceptionMapper extends MainController implements Exception
 
     @Override
     public Response toResponse(NotFoundException e) {
-        return Response.status(404).entity(new ResponseErrorDTO(404, e.getMessage())).build();
+        return this.responseError(404, e.getMessage());
     }
 }
