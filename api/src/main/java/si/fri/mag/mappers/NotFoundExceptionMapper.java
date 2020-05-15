8806ac330b25f36b754ffcfbf8534572e4077acd
Uros Zoretic <uz1734@student.uni-lj.si>
2020-05-15 16:47:52 +0200

    not found exception more verbose

--- api/src/main/java/si/fri/mag/mappers/NotFoundExceptionMapper.java
+++ api/src/main/java/si/fri/mag/mappers/NotFoundExceptionMapper.java
@@ -4,16 +4,22 @@ import si.fri.mag.DTO.responses.ResponseErrorDTO;
 import si.fri.mag.controllers.MainController;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpUtils;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class NotFoundExceptionMapper extends MainController implements ExceptionMapper<NotFoundException> {
+    @Context
+    private HttpServletRequest request;
 
     @Override
     public Response toResponse(NotFoundException e) {
-        return this.responseError(404, e.getMessage());
+        final StringBuffer absolutePath = HttpUtils.getRequestURL(request);
+        return this.responseError(404, e.getMessage() + "  : " + absolutePath);
     }
 }
